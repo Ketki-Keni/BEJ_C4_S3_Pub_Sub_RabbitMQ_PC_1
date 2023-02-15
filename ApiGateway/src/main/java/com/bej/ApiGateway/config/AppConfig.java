d0c20db23ffba561307c394b19410594821123b6
Ketki Keni <sketki220@gmail.com>
2023-02-15 09:24:22 +0530

    changes in AppConfig.java

--- ApiGateway/src/main/java/com/bej/ApiGateway/config/AppConfig.java
+++ ApiGateway/src/main/java/com/bej/ApiGateway/config/AppConfig.java
@@ -9,16 +9,18 @@ package com.bej.ApiGateway.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder routeLocatorBuilder){
         return routeLocatorBuilder.routes()
                 .route(p->p.path("/api/v1/**")
-                        .uri("http://localhost:8082"))
+                        .uri("http://localhost:8082/"))
                 .route(p->p.path("/api/v2/**")
-                        .uri("http://localhost:8083"))
+                        .uri("http://localhost:8083/"))
                 .build();
     }
 }
